65707fbcc12457ac3f56587d43c54efd86dc3e21
MJ Studio <mym0404@gmail.com>
2025-08-30 16:17:55 +0900

    chore: unconscious commit 🎲

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/mj/projects/cc-self-refer/node_modules/.pnpm/infima@0.2.0-alpha.45/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/mj/projects/cc-self-refer/node_modules/.pnpm/@docusaurus+theme-classic@3.8.1_@types+react@19.1.12_esbuild@0.25.9_react-dom@19.1.1_re_6218bc18f15c5626ef6cc4472a83aa36/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/mj/projects/cc-self-refer/node_modules/.pnpm/@docusaurus+theme-classic@3.8.1_@types+react@19.1.12_esbuild@0.25.9_react-dom@19.1.1_re_6218bc18f15c5626ef6cc4472a83aa36/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/mj/projects/cc-self-refer/docs/src/css/custom.css"),
-];
