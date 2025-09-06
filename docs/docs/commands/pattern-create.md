5e9181f4e1d1e576ddac5f35fe7c7b40951352a9
MJ <mym0404@gmail.com>
2025-09-06 12:31:04 +0900

    refactor: remove documentation system dependencies
    Remove docs-related scripts and dependencies from package.json

--- docs/docs/commands/pattern-create.md
+++
@@ -1,6 +0,0 @@
----
-id: pattern-create
-title: Code Pattern - Save Reusable Code Patterns
-sidebar_label: /pattern-create
----
-
--- docs/src/theme/MDXComponents.tsx
+++
@@ -1,7 +0,0 @@
-import MDXComponents from '@theme-original/MDXComponents';
-import CommandDemo from '@site/src/components/CommandDemo';
-
-export default {
-  ...MDXComponents,
-  CommandDemo,
-};
